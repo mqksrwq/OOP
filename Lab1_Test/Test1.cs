e5d6ef90dcd35e41067e686951b4da81d02f8f4c
mqksrwq <maksim4ikb2006@gmail.com>
2026-02-27 00:35:38 +0300

    add unit testing for lab1

---
+++ Lab1_Test/Test1.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab1_Test;
+using Lab1;
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Hotel hotel = new Hotel();
+        
+        Assert.AreEqual("Неизвестно",hotel.Name);
+        Assert.AreEqual(0,hotel.OccupiedRooms);
+        Assert.AreEqual(10,hotel.TotalRooms);
+        Assert.AreEqual(1000m,hotel.PricePerDay);
+        Assert.AreEqual("Адрес не задан",hotel.Address);
+        Assert.AreEqual(3.5,hotel.Rating);
+        Assert.AreEqual(true,hotel.HasFreeWiFi);
+    }
+}
